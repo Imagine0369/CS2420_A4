5128017f8a711666699b2d31fb2c79a4365e800e
mikephelps <mikephelps@mikes-mbp-3.uconnect.utah.edu>
2021-09-19 21:41:58 -0600

    Commit

--- assignment4/src/assign04/AnagramChecker.java
+++ assignment4/src/assign04/AnagramChecker.java
@@ -1,7 +1,13 @@
 package assign04;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class AnagramChecker {
 	
@@ -30,12 +36,15 @@ public class AnagramChecker {
 		
 		for(int i = 1; i < arr.length; i++) {
 			T key = arr[i];
-			while(i != 0 && cmp.compare(key, arr[i]) < 0) {
+			while(i != 0 && cmp.compare(key, arr[i-1]) < 0) {
 				arr[i] = arr[i-1];
 				i--;
 			}
 			arr[i] = key;
 		}
+		
+//		System.out.print("FROM THE METHOD: ");
+//		System.out.println(Arrays.toString(arr));
 	}
 	
 	public static boolean areAnagrams(String str1, String str2) {
@@ -52,23 +61,60 @@ public class AnagramChecker {
 		//get most common annagram
 		//if word the annagram of word in orignal string equals key, add that word to return string
 		
-		String annagramArray
+		String[] copyArr = new String [arr.length];
+		String[] annagramArray = new String [arr.length];
 		HashMap<String, Integer> annaMap = new HashMap<String, Integer>();
+		String mostCommonAnnagram = "";
+		ArrayList<String> uniqueAnagrams = new ArrayList<String>();
 		
-		
-		
-		for(String s : arr) {
-			if(annaMap.containsValue(s)) {
-				annaMap.put(s, annaMap.get(s) + 1);
-			}
-			else{ 
-				annaMap.put(s, 0);
-			}
+		//Copy values of initial array into copyArray
+		for(int i = 0; i < arr.length; i++) {
+			copyArr[i] = arr[i];
 		}
 		
 		
+		//Create new array with initial arrays annagram values
+		for(int j = 0; j < arr.length; j++) {
+			annagramArray[j] = sort(arr[j]);
+		}
+		System.out.print("Anagram Array: ");
+		System.out.println(Arrays.toString(annagramArray));
 		
 		
+		//Populate hashmap with annagram count
+		for(String s : annagramArray) {
+			if(annaMap.containsKey(s)) {
+				annaMap.replace(s, (annaMap.get(s)) + 1);
+			}
+			else{ 
+				annaMap.put(s, 0);
+				uniqueAnagrams.add(s);
+			}
+		}
+	
+		mostCommonAnnagram = uniqueAnagrams.get(0);
+		for(String s : uniqueAnagrams) {
+			if(annaMap.get(s) > annaMap.get(mostCommonAnnagram)) {
+				s = mostCommonAnnagram;
+			}
+		}
+			uniqueAnagrams.clear();
+			
+			for(int l = 0; l < copyArr.length; l++) {
+				if(sort(copyArr[l]).equals(mostCommonAnnagram)) {
+					uniqueAnagrams.add(copyArr[l]);
+				}
+			}
+		
+			String[] returnArr = new String[uniqueAnagrams.size()];
+		for(int m = 0; m < uniqueAnagrams.size(); m++) {
+			returnArr[m] = uniqueAnagrams.get(m);
+		}
+		
+		return returnArr;
+		
+		
+//		
 //		String[] temp = new String[arr.length];
 //		for(int i = 0; i < arr.length; i++) {
 //			temp[i] = arr[i];
@@ -82,13 +128,33 @@ public class AnagramChecker {
 //		getMostFrequent(arr);
 	}
 	
-	public static String getMostFrequent(String[] str) {
-		int counter = 0;
+	
+//	public static String getMostFrequent(String[] str) {
+//		int counter = 0;
+//		
+//		
+//	}
+	
+	public static String[] readWordsFile(String filename) throws FileNotFoundException {
+		ArrayList<String> wordList = new ArrayList<String>();
 		
+		File file = new File(filename);
+		Scanner scan = new Scanner(file, "utf-8");	
 		
+		while (scan.hasNext()) {
+			wordList.add(scan.next());
+		}
+		scan.close();
+		System.out.println(wordList.toString());
+		
+		String [] returnList = new String [wordList.size()];
+		for(int i = 0; i < wordList.size(); i++) {
+			returnList[i] = wordList.get(i);
+		}
+		return returnList;
 	}
 	
-	protected class OrderByString implements Comparator<String> {
+	protected static class OrderByString implements Comparator<String> {
 
 		@Override
 		public int compare(String o1, String o2) {
@@ -96,12 +162,21 @@ public class AnagramChecker {
 		}
 	}
 	
+
 	public static void main(String[] args) {
-		String[] testArr = {"Art" , "Tar", "Hat", "crates", "rack", "reacts", "Caters", "caster", "car"};
-		String test = "";
-		String testB = "";
-		System.out.println(areAnagrams(test, testB));
-		//System.out.println(sort(testB));
+//		String[] testArr = {"Art" , "Tar", "Car", "Zap", "Apple", "Caters", "caster", "Reacts", "carets"};
+//		String test = "";
+//		String testB = "bazza";
+//		System.out.println(areAnagrams(test, testB));
+//		System.out.println(Arrays.toString(getLargestAnagramGroup(testArr)));
+//		System.out.println(sort(testB));
+		try {
+			readWordsFile("src/assign04/sample_word_list.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
