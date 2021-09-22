1ba32fadc57ca70ecdbfc158b3d138a6d6cc88ab
Imagine0369 <77407532+Imagine0369@users.noreply.github.com>
2021-09-21 22:25:54 -0600

    Update AnagramCheckerTest.java

--- assignment4/src/assign04/AnagramCheckerTest.java
+++ assignment4/src/assign04/AnagramCheckerTest.java
@@ -9,9 +9,15 @@ import org.junit.jupiter.api.Test;
 
 import assign04.AnagramChecker.OrderByAnagram;
 
+/**
+ * Junit Tests for AnagramChecker class and its methods.
+ * 
+ * @author Mike Phelps and Seth Polevoi
+ *
+ */
 class AnagramCheckerTest {
-	
-	//------------------------- sort tests starts---------------------------
+
+	// ------------------------- sort tests starts---------------------------
 	@Test
 	void testSortTwoCharacters() {
 		assertEquals("ab", AnagramChecker.sort("ba"));
@@ -19,6 +25,7 @@ class AnagramCheckerTest {
 		assertEquals("pq", AnagramChecker.sort("pq"));
 		assertEquals("gy", AnagramChecker.sort("yg"));
 	}
+
 	@Test
 	void testSortSimpleCase() {
 		assertEquals("hjno", AnagramChecker.sort("john"));
@@ -26,6 +33,7 @@ class AnagramCheckerTest {
 		assertEquals("ddeloo", AnagramChecker.sort("doodle"));
 		assertEquals("abcde", AnagramChecker.sort("edcba"));
 	}
+
 	@Test
 	void testSortPreSorted() {
 		assertEquals("hjno", AnagramChecker.sort("hjno"));
@@ -33,6 +41,7 @@ class AnagramCheckerTest {
 		assertEquals("ddeloo", AnagramChecker.sort("ddeloo"));
 		assertEquals("abcde", AnagramChecker.sort("abcde"));
 	}
+
 	@Test
 	void testSortCapitals() {
 		assertEquals("Jhno", AnagramChecker.sort("John"));
@@ -40,10 +49,12 @@ class AnagramCheckerTest {
 		assertEquals("DDELOO", AnagramChecker.sort("DOODLE"));
 		assertEquals("Eabcd", AnagramChecker.sort("Edcba"));
 	}
+
 	@Test
 	void testSortEmpty() {
 		assertEquals("", AnagramChecker.sort(""));
 	}
+
 	@Test
 	void testSortWithNumbers() {
 		assertEquals("7Jhno", AnagramChecker.sort("Jo7hn"));
@@ -51,172 +62,222 @@ class AnagramCheckerTest {
 		assertEquals("34ddeloo", AnagramChecker.sort("d4oo3dle"));
 		assertEquals("03abcde", AnagramChecker.sort("3edcba0"));
 	}
-	//------------------------- sort tests ends-----------------------------
-	
-	
-	//------------------------- insertionSort tests starts---------------------------
+	// ------------------------- sort tests ends-----------------------------
+
+	// ------------------------- insertionSort tests
+	// starts---------------------------
 	@Test
 	void testInsertionSortInteger() {
-		Integer[] testArr = {2,6,4,1,-1,7,8,0,-3,89,4};
-		
+		Integer[] testArr = { 2, 6, 4, 1, -1, 7, 8, 0, -3, 89, 4 };
+
 		Comparator<Integer> cmp = new OrderByInteger();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
-		Integer[] testVals = {-3,-1,0,1,2,4,4,6,7,8,89};
+		AnagramChecker.insertionSort(testArr, cmp);
+
+		Integer[] testVals = { -3, -1, 0, 1, 2, 4, 4, 6, 7, 8, 89 };
 		assertArrayEquals(testVals, testArr);
-		
+
 	}
+
 	@Test
 	void testInsertionSortAlpha() {
-		String[] testArr = {"art" , "tar", "hat", "crates", "rack", "reacts", "caters", "caster", "car"};
-		
+		String[] testArr = { "art", "tar", "hat", "crates", "rack", "reacts", "caters", "caster", "car" };
+
 		Comparator<String> cmp = new OrderByAlpha();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
-		String[] testVals = {"art","car","caster", "caters","crates","hat","rack", "reacts","tar"};
+		AnagramChecker.insertionSort(testArr, cmp);
+
+		String[] testVals = { "art", "car", "caster", "caters", "crates", "hat", "rack", "reacts", "tar" };
 		assertArrayEquals(testVals, testArr);
 	}
+
 	@Test
 	void testInsertionSortAnagram() {
-		String[] testArr = {"cat","tab","join","bat","zzzzz"};
-		
+		String[] testArr = { "cat", "tab", "join", "bat", "zzzzz" };
+
 		Comparator<String> cmp = new OrderByAnagram();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
-		String[] testVals = {"tab","bat","cat", "join","zzzzz"};
+		AnagramChecker.insertionSort(testArr, cmp);
+
+		String[] testVals = { "tab", "bat", "cat", "join", "zzzzz" };
 		assertArrayEquals(testVals, testArr);
 	}
+
 	@Test
 	void testInsertionSortAnagramWithNumbers() {
-		String[] testArr = {"ca3t","3tab2","dans","da2ns","jo84in","ba21t","zzzz0zzz","dans8"};
-		
+		String[] testArr = { "ca3t", "3tab2", "dans", "da2ns", "jo84in", "ba21t", "zzzz0zzz", "dans8" };
+
 		Comparator<String> cmp = new OrderByAnagram();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
-		String[] testVals = {"zzzz0zzz","ba21t","3tab2","da2ns", "ca3t","jo84in","dans8","dans"};
+		AnagramChecker.insertionSort(testArr, cmp);
+
+		String[] testVals = { "zzzz0zzz", "ba21t", "3tab2", "da2ns", "ca3t", "jo84in", "dans8", "dans" };
 		assertArrayEquals(testVals, testArr);
 	}
+
 	@Test
 	void testInsertionSortAnagramWithCapitals() {
-		String[] testArr = {"cAt","TaB","join","Cat","joIn","bat","CAT","jOOhns"};
-		
+		String[] testArr = { "cAt", "TaB", "join", "Cat", "joIn", "bat", "CAT", "jOOhns" };
+
 		Comparator<String> cmp = new OrderByAnagram();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
-		String[] testVals = {"CAT", "cAt", "TaB", "Cat", "joIn", "jOOhns", "bat", "join"};
+		AnagramChecker.insertionSort(testArr, cmp);
+
+		String[] testVals = { "CAT", "cAt", "TaB", "Cat", "joIn", "jOOhns", "bat", "join" };
 		assertArrayEquals(testVals, testArr);
 	}
+
 	@Test
 	void testInsertionSortEmptyArray() {
 		String[] testArr = new String[0];
-		
+
 		Comparator<String> cmp = new OrderByAnagram();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
+		AnagramChecker.insertionSort(testArr, cmp);
+
 		String[] testVals = {};
 		assertArrayEquals(testVals, testArr);
 	}
+
 	@Test
 	void testInsertionSortPreSorted() {
-		String[] testArr = {"tab","bat","cat","cat", "cat", "joohns","join","join"};
-		
+		String[] testArr = { "tab", "bat", "cat", "cat", "cat", "joohns", "join", "join" };
+
 		Comparator<String> cmp = new OrderByAnagram();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
-		String[] testVals = {"tab","bat","cat","cat", "cat", "joohns","join","join"};
+		AnagramChecker.insertionSort(testArr, cmp);
+
+		String[] testVals = { "tab", "bat", "cat", "cat", "cat", "joohns", "join", "join" };
 		assertArrayEquals(testVals, testArr);
 	}
+
 	@Test
 	void testInsertionSortSameValues() {
-		String[] testArr = {"TaB","bat","cAt","Cat","TaB","bat","cAt","Cat",};
-		
+		String[] testArr = { "TaB", "bat", "cAt", "Cat", "TaB", "bat", "cAt", "Cat", };
+
 		Comparator<String> cmp = new OrderByAnagram();
-		AnagramChecker.insertionSort(testArr, cmp );
-		
-		String[] testVals = {"cAt","cAt","TaB","TaB","Cat","Cat","bat","bat"};
+		AnagramChecker.insertionSort(testArr, cmp);
+
+		String[] testVals = { "cAt", "cAt", "TaB", "TaB", "Cat", "Cat", "bat", "bat" };
 		assertArrayEquals(testVals, testArr);
 	}
-	//------------------------- insertionSort tests ends-----------------------------
-	
-	
-	//------------------------- areAnagrams tests starts---------------------------
+	// ------------------------- insertionSort tests
+	// ends-----------------------------
+
+	// ------------------------- areAnagrams tests starts---------------------------
 	@Test
 	void testAreAnagramsStandard() {
-		assertTrue( AnagramChecker.areAnagrams("done","node") );
+		assertTrue(AnagramChecker.areAnagrams("done", "node"));
 	}
+
 	@Test
 	void testAreAnagramsWithCaps() {
-		assertTrue( AnagramChecker.areAnagrams("doNe","NODE") );
+		assertTrue(AnagramChecker.areAnagrams("doNe", "NODE"));
 	}
+
 	@Test
 	void testAreAnagramsEmpty() {
-		assertTrue( AnagramChecker.areAnagrams("","") );
+		assertTrue(AnagramChecker.areAnagrams("", ""));
 	}
+
 	@Test
 	void testAreAnagramsSameWord() {
-		assertTrue( AnagramChecker.areAnagrams("done","done") );
+		assertTrue(AnagramChecker.areAnagrams("done", "done"));
 	}
+
 	@Test
 	void testAreAnagramsFalse() {
-		assertFalse( AnagramChecker.areAnagrams("john","NODE") );
+		assertFalse(AnagramChecker.areAnagrams("john", "NODE"));
 	}
+
 	@Test
 	void testAreAnagramsWithNumbers() {
-		assertTrue( AnagramChecker.areAnagrams("0899","9890") );
+		assertTrue(AnagramChecker.areAnagrams("0899", "9890"));
 	}
-	//------------------------- areAnagrams tests ends-----------------------------
-	
-	
-	//------------------------- getLargestAnagramGroup tests starts---------------------------
+	// ------------------------- areAnagrams tests ends-----------------------------
+
+	// ------------------------- getLargestAnagramGroup tests
+	// starts---------------------------
 	@Test
 	void testGetLargestAnagramGroupOneGroup() {
-		String[] testArr = {"cat","tab","join","bat"};
-		String[] testVals = {"tab", "bat"};
-		assertArrayEquals( testVals, AnagramChecker.getLargestAnagramGroup(testArr) );
+		String[] testArr = { "cat", "tab", "join", "bat" };
+		String[] testVals = { "tab", "bat" };
+		assertArrayEquals(testVals, AnagramChecker.getLargestAnagramGroup(testArr));
 	}
+
 	@Test
 	void testGetLargestAnagramGroupTwoGroups() {
-		String[] testArr = {"caster","tab","join","bat","crates","steacr","ratesc"};
-		String[] testVals = {"caster", "crates", "steacr", "ratesc"};
-		assertArrayEquals( testVals, AnagramChecker.getLargestAnagramGroup(testArr) );
+		String[] testArr = { "caster", "tab", "join", "bat", "crates", "steacr", "ratesc" };
+		String[] testVals = { "caster", "crates", "steacr", "ratesc" };
+		assertArrayEquals(testVals, AnagramChecker.getLargestAnagramGroup(testArr));
 	}
+
 	@Test
 	void testGetLargestAnagramGroupNoAnagrams() {
-		String[] testArr = {"cat","tab","join","ba3t","Caat","cate","cant"};
+		String[] testArr = { "cat", "tab", "join", "ba3t", "Caat", "cate", "cant" };
 		String[] testVals = new String[0];
-		assertArrayEquals( testVals, AnagramChecker.getLargestAnagramGroup(testArr) );
+		assertArrayEquals(testVals, AnagramChecker.getLargestAnagramGroup(testArr));
 	}
+
 	@Test
 	void testGetLargestAnagramGroupCapitals() {
-		String[] testArr = {"castEr","Tab","joIn","bat","crAtes","steacr","ratesc","Abt"};
-		String[] testVals = {"castEr", "crAtes", "steacr", "ratesc"};
-		System.out.println( Arrays.toString(testArr) );
-		
-		System.out.println( Arrays.toString(AnagramChecker.getLargestAnagramGroup(testArr)) );
+		String[] testArr = { "castEr", "Tab", "joIn", "bat", "crAtes", "steacr", "ratesc", "Abt" };
+		String[] testVals = { "castEr", "crAtes", "steacr", "ratesc" };
+
+		assertArrayEquals(testVals, AnagramChecker.getLargestAnagramGroup(testArr));
 	}
-	//------------------------- getLargestAnagramGroup tests ends-----------------------------
-	
-	protected static class OrderByAnagram implements Comparator<String>{
+	// ------------------------- getLargestAnagramGroup tests
+	// end-----------------------------
+
+	// ------------------------- getLargestAnagramGroup(file) tests
+	// start----------------------
+	@Test
+	void testGetLargestAnagramGroupWithNormalFile() {
+		String[] expectedVals = { "allergy", "gallery", "largely" };
+		assertArrayEquals(expectedVals, AnagramChecker.getLargestAnagramGroup("src/assign04/anagrams1.txt"));
+	}
+
+	@Test
+	void testGetLargestAnagramGroupEmptyFile() {
+		String[] emptyArr = new String[0];
+		assertArrayEquals(emptyArr, AnagramChecker.getLargestAnagramGroup("src/assign04/emptytxtfile.txt"));
+	}
+
+	@Test
+	void testGetLargestAnagramGroupFileNotFound() {
+		String[] emptyArr = new String[0];
+		assertArrayEquals(emptyArr, AnagramChecker.getLargestAnagramGroup("src/assign04/fileDNE.txt"));
+	}
+
+	@Test
+	void testGetLargestAnagramGroupFileWithTextGaps() {
+		String[] expectedVals = { "door", "odor" };
+		assertArrayEquals(expectedVals, AnagramChecker.getLargestAnagramGroup("src/assign04/testToLines.txt"));
+	}
+
+	@Test
+	void testGetLargestAnagramGroupIncompatibleFile() {
+		String[] emptyArr = new String[0];
+		assertArrayEquals(emptyArr, AnagramChecker.getLargestAnagramGroup("src/assign04/Anaylsis Document.docx"));
+	}
+
+	// ------------------------- getLargestAnagramGroup(file) tests
+	// end------------------------
+
+	protected static class OrderByAnagram implements Comparator<String> {
 
 		@Override
 		public int compare(String o1, String o2) {
-			return AnagramChecker.sort(o1).compareTo( AnagramChecker.sort(o2) );
+			return AnagramChecker.sort(o1).compareTo(AnagramChecker.sort(o2));
 		}
 	}
-	
-	protected static class OrderByInteger implements Comparator<Integer>{
+
+	protected static class OrderByInteger implements Comparator<Integer> {
 
 		@Override
 		public int compare(Integer o1, Integer o2) {
-			return o1-o2;
+			return o1 - o2;
 		}
 	}
-	
-	protected static class OrderByAlpha implements Comparator<String>{
+
+	protected static class OrderByAlpha implements Comparator<String> {
 
 		@Override
 		public int compare(String o1, String o2) {
-			return o1.compareTo( o2 );
+			return o1.compareTo(o2);
 		}
 	}
 
